04973c531da7f4f7375b27c663589feac3326dce
NgMinhDucc <ducnm@gdscptit.dev>
2026-09-01 23:48:30 +0700

    Prevented lost-update risk in update_projects - Sep 01 2026

--- src/routers/projects.py
+++ src/routers/projects.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, status
 
-from auth import CurrentUser, CurrentProject, AllProjects, get_role
+from auth import CurrentUser, CurrentProject, CurrentProjectForUpdate, AllProjects, get_role
 from database import SessionDep
 from models import Projects, CreateProject, UpdateProject, ProjectPublic, ProjectsAssignments
 
@@ -12,7 +12,7 @@ async def create_project(session: SessionDep, current_user: CurrentUser, create_
     new_project = Projects(**project_data)
     
     session.add(new_project)
-    session.flush() # temporary data
+    session.flush() #* temporary data
     
     if current_user.user_id is None or new_project.project_id is None:
         raise HTTPException(
@@ -27,28 +27,27 @@ async def create_project(session: SessionDep, current_user: CurrentUser, create_
     )
     
     session.add(project_owned_by)
-    session.commit() # permanent data
+    session.commit() #* permanent data
     
     session.refresh(new_project)
     session.refresh(project_owned_by)
     
     return new_project
 
-@router.patch("/update-projects/{project_name}", response_model=ProjectPublic) # fix lost update risk
+@router.patch("/update-projects/{project_name}", response_model=ProjectPublic)
 async def update_projects(
     session: SessionDep,
     current_user: CurrentUser,
-    current_project: CurrentProject,
+    current_project_for_update: CurrentProjectForUpdate,
     update_project: UpdateProject
 ):
-    if current_user.user_id is None or current_project.project_id is None:
+    if current_user.user_id is None or current_project_for_update.project_id is None:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="User ID or Project ID is missing"
         )
         
-    role = get_role(session, current_user.user_id, current_project.project_id)
-    print(role)
+    role = get_role(session, current_user.user_id, current_project_for_update.project_id)
     if role != "OWNER":
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
@@ -56,15 +55,15 @@ async def update_projects(
         )
     
     updated_data = update_project.model_dump(exclude_unset=True)
-    current_project.sqlmodel_update(updated_data)
+    current_project_for_update.sqlmodel_update(updated_data)
     
-    session.add(current_project)
+    session.add(current_project_for_update)
     session.commit()
-    session.refresh(current_project)
+    session.refresh(current_project_for_update)
     
-    return current_project
+    return current_project_for_update
 
-@router.get("/my-projects", response_model=ProjectPublic) # need fix, error is in response_model
+@router.get("/my-projects", response_model=ProjectPublic) #! ValueError caused by response_model and AllProjects
 async def get_projects(current_user: CurrentUser, all_projects: AllProjects):
     return all_projects
 
@@ -72,4 +71,4 @@ async def get_projects(current_user: CurrentUser, all_projects: AllProjects):
 async def get_project(current_user: CurrentUser, current_project: CurrentProject):
     return current_project
 
-# need a delete project api
+# todo: add a delete project api
