3d8b12f79b51e7fcad65c013bba8c8e09a3a7d39
NgMinhDucc <ducnm@gdscptit.dev>
2026-09-21 00:58:29 +0700

    I don't remember what I've fixed in this - Sep 20 2026

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,5 @@
 from .users import Users, CreateUser, UserPublic, UpdateUser, ChangePassword
-from .token import Token
+from .token import Token, TokenData
 from .projects import Projects, CreateProject, ProjectPublic, ProjectsAssignments, UpdateProject, ProjectPaginationInfo
 from .tasks import Tasks, CreateTask, UpdateTask, TaskPublic
 from .comments import Comments, CreateComment
