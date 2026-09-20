a05fdccd19b071df4389463bc3dcd3fef5a09f6a
NgMinhDucc <ducnm@gdscptit.dev>
2026-09-21 00:49:33 +0700

    Some tiny fixes - Sep 21 2026

--- src/auth.py
+++ src/auth.py
@@ -10,8 +10,8 @@ import os
 from dotenv import load_dotenv
 
 from database import SessionDep
-import models.users as mu
-import models.token as mtok
+from models import Users
+from models import TokenData
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login") # note: the parameter is only useful in swagger ui
 Tokenn = Annotated[str, Depends(oauth2_scheme)]
@@ -28,7 +28,7 @@ def verify_password(plain_password, hashed_password):
 
 def get_user(session: SessionDep, user_name: str):
     # find the first account whose username matches with the input
-    user = session.exec(select(mu.Users).where(mu.Users.user_name == user_name)).first()
+    user = session.exec(select(Users).where(Users.user_name == user_name)).first()
     return user
 
 def authenticate_user(session: SessionDep, user_name: str, password: str):
@@ -65,7 +65,7 @@ def get_current_user(session: SessionDep, token: Tokenn):
         user_name = payload.get("sub")
         if not user_name:
             raise credentials_exception
-        token_data = mtok.TokenData(user_name=user_name)
+        token_data = TokenData(user_name=user_name)
     except InvalidTokenError:
         raise credentials_exception
     
@@ -73,9 +73,9 @@ def get_current_user(session: SessionDep, token: Tokenn):
     if user is None:
         raise credentials_exception
     return user
-        
+
 FormData = Annotated[OAuth2PasswordRequestForm, Depends()]
 
-CurrentUser = Annotated[mu.Users, Depends(get_current_user)]
+CurrentUser = Annotated[Users, Depends(get_current_user)]
 
-SearchedUser = Annotated[mu.Users, Depends(get_user)]
+SearchedUser = Annotated[Users, Depends(get_user)]
